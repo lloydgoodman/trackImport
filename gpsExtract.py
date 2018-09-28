215fb18de0edefeb2b364e8eecd739a022779724
Lloyd Goodman <43635437+lloydgoodman@users.noreply.github.com>
2018-09-28 22:48:40 +0200

    Add existing file

---
+++ gpsExtract.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3.6
+ 
+import sys
+import untangle
+
+if len(sys.argv) < 2:
+	print('Usage: gpsExtract.py <xmlfile.xml>')
+	sys.exit()
+
+xmlInput = sys.argv[1]
+
+obj = untangle.parse(xmlInput)
+for note in obj.note:
+    body = note.body.cdata
+
+print(body)
